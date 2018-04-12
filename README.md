e964086d9b64498f054ca21e24d37d708087c5eb
Simon Levesque <simon@simonlevesque.com>
2018-04-12 07:42:47 -0400

    DumpAllSystemErr - To display the config and all the messages properties to stderr

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # About
 
 Some Mailets and Matchers for Apache James
+
+# Mailet - DumpAllSystemErr
+
+## Description
+
+This is used to display the config and all the messages properties to stderr.
+
+## Configuration
+
+In *mailetcontainer.xml*, you can add it at any stage with: 
+
+```
+<mailet match="All" class="com.foilen.james.components.mailet.DumpAllSystemErr" />
+```
---
+++ src/main/java/com/foilen/james/components/DumpAllSystemErr.java
@@ -0,0 +1,112 @@
+/*
+    James Extra Components
+    https://github.com/foilen/james-extra-components
+    Copyright (c) 2018 Foilen (http://foilen.com)
+
+    The MIT License
+    http://opensource.org/licenses/MIT
+
+ */
+package com.foilen.james.components;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.inject.Inject;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import javax.sql.DataSource;
+
+import org.apache.mailet.Mail;
+import org.apache.mailet.MailetConfig;
+import org.apache.mailet.MailetContext;
+import org.apache.mailet.base.GenericMailet;
+
+public class DumpAllSystemErr extends GenericMailet {
+
+    protected DataSource datasource;
+
+    @Override
+    public String getMailetInfo() {
+        return "Dumps config and message to System.err";
+    }
+
+    @Override
+    public void init() throws MessagingException {
+        super.init();
+
+        System.err.println("---[Mailet Config]---");
+        MailetConfig mailetConfig = getMailetConfig();
+        System.err.println("Mailet Name: " + mailetConfig.getMailetName());
+
+        System.err.println("---[Mailet Config - Init Parameters ]---");
+        mailetConfig.getInitParameterNames().forEachRemaining(name -> {
+            System.err.println(name + " -> " + mailetConfig.getInitParameter(name));
+        });
+
+        System.err.println("---[Mailet Config - Context ]---");
+        MailetContext mailetContext = mailetConfig.getMailetContext();
+
+        System.err.println("---[Mailet Config - Context - Attributes]---");
+        mailetContext.getAttributeNames().forEachRemaining(name -> {
+            System.err.println(name + " -> " + mailetContext.getAttribute(name));
+        });
+
+        System.err.println("---[Datasource]---");
+        if (datasource == null) {
+            System.err.println("Is null");
+        } else {
+            System.err.println("Type -> " + datasource.getClass());
+
+            System.err.println("Tables : ");
+            try (Connection connection = datasource.getConnection()) {
+                ResultSet rs = connection.createStatement().executeQuery("show tables");
+                while (rs.next()) {
+                    System.err.println("\t" + rs.getString(1));
+                }
+            } catch (SQLException e) {
+                throw new MessagingException("Problem getting the table list", e);
+            }
+
+        }
+
+        System.err.println("------------");
+    }
+
+    @Override
+    public void service(Mail mail) throws MessagingException {
+        try {
+
+            // Attributes
+            System.err.println("---[Mail - Attributes]---");
+            mail.getAttributeNames().forEachRemaining(name -> {
+                System.err.println(name + " -> " + mail.getAttribute(name));
+            });
+
+            System.err.println("---[Mail - Sender]---");
+            System.err.println("Email: " + mail.getSender().asString());
+
+            // Recipients
+            System.err.println("---[Mail - Recipients]---");
+            mail.getRecipients().forEach(it -> {
+                System.err.println(it.asString());
+            });
+
+            // Message
+            System.err.println("---[Mail - Message]---");
+            MimeMessage message = mail.getMessage();
+            message.writeTo(System.err);
+            System.err.println("------------");
+        } catch (IOException ioe) {
+            log("error printing message", ioe);
+        }
+    }
+
+    @Inject
+    public void setDataSource(DataSource datasource) {
+        this.datasource = datasource;
+    }
+
+}
