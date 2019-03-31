a776644ab90504c8f5b561d1de8929cde95c9763
Simon Levesque <simon@foilen.com>
2019-03-31 19:37:46 -0400

    Update copyright

--- src/main/java/com/foilen/james/components/mailet/ExactAndCatchAllRedirections.java
+++ src/main/java/com/foilen/james/components/mailet/ExactAndCatchAllRedirections.java
@@ -1,7 +1,7 @@
 /*
     James Extra Components
     https://github.com/foilen/james-extra-components
-    Copyright (c) 2018 Foilen (http://foilen.com)
+    Copyright (c) 2018-2019 Foilen (http://foilen.com)
 
     The MIT License
     http://opensource.org/licenses/MIT
